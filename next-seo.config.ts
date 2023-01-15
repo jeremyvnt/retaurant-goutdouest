b79b7cecd1f6bf3e1be8ac4cfdef072f9b7ea688
Jérémy Vincent <jeremy.vincent@h4p.fr>
2023-01-15 22:31:54 +0100

    fix(_app): try to fix image missing

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -9,7 +9,7 @@ const nextSeoConfiguration = {
     site_name: "Restaurant Goût d'Ouest",
     images: [
       {
-        url: 'https://restaurant-goutdouest.netlify.app/images/table.jpg?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1926&amp;q=80',
+        url: 'images/table.jpg',
         width: 800,
         height: 600,
         alt: "Table au restaurant Goût d'Ouest",
